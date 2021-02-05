2874a30c293f0c63c86b091946aec1a0edc55157
ervin-mitra <ervin.mitra@gopuff.com>
2021-02-04 22:08:19 -0800

    added another character

--- src/App.vue
+++ src/App.vue
@@ -18,12 +18,12 @@
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  button {
+    background: #009435;
+    border: 1px solid #009435;
+  }
+
+  .small-container {
+    max-width: 680px;
+  }
 </style>
